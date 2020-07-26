fa1c10c9389e0b91b2de6a404d0d0912d0d209db
Uday Chugh <61309187+udaychugh@users.noreply.github.com>
2020-07-26 15:47:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+$(window).on("load resize scroll", function() {
+  $(".bg-static").each(function() {
+    var windowTop = $(window).scrollTop();
+    var elementTop = $(this).offset().top;
+    var leftPosition = windowTop - elementTop;
+      $(this)
+        .find(".bg-move")
+        .css({ left: leftPosition });
+  });
+});
 
+
+
+
+
+
+
+
+
+function parallax()
+{
+  var image= document.getElementById('image2');
+  image.style.right= (window.pageYOffset/ 5) + 'px';
+
+} 
+window.addEventListener("scroll",parallax,false)
